d01035ace9ad5563fc262375731ba71ec3b861d1
Bhone Pyae Moie <bhonepyae.it@gmail.com>
2025-04-16 22:52:17 +0700

    Database Insert

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Blog = require("./Models/blog");
+const path = require("path");
+const bodyParser = require("body-parser");
+
 let mongoUrl =
   "mongodb+srv://moegyi2442:P4ter2442@blog.zjobhiv.mongodb.net/?retryWrites=true&w=majority&appName=Blog";
 
@@ -21,4 +25,23 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/blog", (req, res) => {
+  res.sendFile("./views/blog.html", { root: __dirname });
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/submit", async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const blog = new Blog({ title, content });
+    await blog.save();
+    res.status(201).json(blog);
+  } catch (err) {
+    console.error("Error saving blog:", err);
+    res.status(500).json({ error: err.message });
+  }
 });
